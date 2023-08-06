add711784fb2835a9f6fc79df286d497a6b33a44
owen <owen.chosen@gmail.com>
2023-08-06 23:26:48 +0100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const pText = document.querySelector("#text");
+    pText.textContent = "This is really cool!";
+  });
